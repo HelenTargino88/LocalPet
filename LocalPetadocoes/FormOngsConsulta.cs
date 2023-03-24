a80dcdc749a80b2ca4d67c28a62ca160c83d776c
HELEN NASCIMENTO TARGINO <helenntragino@gmail.com>
2023-03-24 09:26:37 -0300

    form consulta

--- LocalPetadocoes/FormOngsConsulta.cs
+++ LocalPetadocoes/FormOngsConsulta.cs
@@ -20,9 +20,13 @@ namespace LocalPetadocoes
 
         private void btnAdiconar_Click(object sender, EventArgs e)
         {
-            Ongs ongs = new Ongs(txtNome.Text, txtCnpj.Text, txtEmail.Text, txtDescricao.Text);
+            Ongs ongs = new Ongs(txtNome.Text, txtCnpj.Text, txtCpf.Text, txtEmail.Text, txtDescricao.Text);
             ongs.Inserir();
             txtId.Text = ongs.Id.ToString();
+
+            EnderecoOng = new EnderecoOng(txtCep.Text, txtLogradouro.Text, 
+                txtNumeroEnd.Text, txtComplemento.Text,txtBairro.Text, 
+                txtCidade.Text, cmbUf.Text, cmbEstado.Text, cbTipoEndereco.Text);
         }
 
         private void btnEditar_Click(object sender, EventArgs e)
@@ -30,6 +34,11 @@ namespace LocalPetadocoes
             Ongs ongs = new Ongs(int.Parse(txtId.Text), txtNome.Text, txtCnpj.Text, txtCep.Text, txtDescricao.Text, /**/,/**/);
             ongs.Editar();
             MessageBox.Show("Ong atualizada com sucesso!");
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 }
